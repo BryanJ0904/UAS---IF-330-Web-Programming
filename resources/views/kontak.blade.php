0e0ff27c16a4969a4100909624cd7a33ea743fd7
BryanJ0904 <bryanjonathan2004@gmail.com>
2023-12-11 13:31:22 +0700

    update

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kontak Kami</title>
-    <link rel="stylesheet" href="{{ asset('css/kontak.css') }}" />
+    <link rel="stylesheet" href="{{ asset('kontak.css') }}" />
 </head>
 <body>
     @include('tampilan.main')
